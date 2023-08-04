5bb4ac87b6985383bcf4745515de765721b95f75
ishubham1995 <shubham.malik@crowdbotics.com>
2023-08-04 15:28:09 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "dcxv-dev-107108",
-  url: "https://dcxv-dev-107108.botics.co",
-  api: "https://dcxv-dev-107108.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "dcxv-dev-107108",
+  "url": "https://dcxv-dev-107108.botics.co",
+  "api": "https://dcxv-dev-107108.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
